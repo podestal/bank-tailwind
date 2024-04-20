784d447e5ead4dce39618da26ce77228347a45d9
podestal <luisrodriguezpodesta@gmail.com>
2024-04-20 11:15:56 -0400

    navbar mobile responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,6 +8,7 @@ import { logo, menu, close } from '../assets'
 const Navbar = () => {
 
   const [active, setActive] = useState('')
+  const [toggle, setToggle] = useState(false)
 
   return (
     <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
@@ -21,11 +22,11 @@ const Navbar = () => {
           }}
         >
           <img src={logo} alt="Logo" className='w-9 h-9 object-contain' />
-          <p className='text-white text-[18px] font-bold cursor-pointer'>Adrian 
-            <span className='sm:block hidden'>| JavaScript Mastery</span>
+          <p className='text-white text-[18px] font-bold cursor-pointer flex'>Adrian &nbsp;
+            <span className='sm:block hidden'>| JS Mastery</span>
           </p>
         </Link>
-        <ul className='list-none hideen sm:flex flex-row gap-10'>
+        <ul className='list-none hidden sm:flex flex-row gap-10'>
           {navLinks.map( link => (
             <li 
               key={link.id} 
@@ -36,6 +37,30 @@ const Navbar = () => {
             </li>
           ))}
         </ul>
+        <div className='sm:hidden flex flex-1 justify-end items-center'>
+          <img 
+            src={toggle ? close : menu} 
+            alt='menu' 
+            className='w-[28px] h-[28px] object-contain cursor-pointer'
+            onClick={() => setToggle(!toggle)}
+          />
+          <div className={`${!toggle ? 'hidden' : 'flex'} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w[140px] z-10 rounded-xl`}>
+            <ul className='list-none flex justify-end items-start flex-col gap-4'>
+              {navLinks.map( link => (
+                <li 
+                  key={link.id} 
+                  className={`${active === link.title ? 'text-white' : 'text-secondary'} hover:text-white font-poppins font-medium cursor-pointer text-[16px]`}
+                  onClick={() => {
+                    setActive(link.title)
+                    setToggle(!toggle)
+                  }}
+                >
+                  <a href={`#${link.id}`}>{link.title}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
     </nav>
   )
